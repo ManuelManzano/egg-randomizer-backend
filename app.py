2c55461a5ea3a2de204c732d4c68162faf398363
Manuel Manzano <manuelalejandromanzano@gmail.com>
2025-04-10 00:22:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def generate_egg_seed():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print("Starting server on port:", port)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
